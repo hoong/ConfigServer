72c3581f3efed649a85893571f0d145ed9bf831b
hoong <honggw@outlook.com>
2012-10-22 19:50:57 +0800

    增加业务部分编码，修改设计文档接口部分

--- service_config.cpp
+++ service_config.cpp
@@ -1,4 +1,4 @@
-#include "service.h"
+#include "service_config.h"
 #include "db_mysql.h"
 #include "json_man.h"
 
@@ -35,6 +35,108 @@ int service_config_update::run()
 };
 
 
+
+
+service_instances& service_mgr::inst(const std::string& service)
+{
+	std::map<std::string,service_instances>::iterator it = data.find(service);
+	if(it == data.end())
+	{
+		boost::scoped_lock lock(m_mutex);
+		service_instances si;
+		std::pair<std::map<std::string,service_instances>::iterator,bool> ret =
+			data.insert(std::pair<std::string,service_instances>(service,si));
+		return ret.first->second;
+	}
+	return it->second;
+
+};
+
+void service_mgr::iterate(iterate_f*)
+{
+	std::map<std::string,service_instances>::iterator it;
+	for(it=data.first();it!=data.end();++it)
+		iterate_f->doit(*it);
+};
+
+int service_instances::insert(const std::string& addr,boost::shared_ptr<base::net::handler> ptr)
+{
+	boost::scoped_lock lock(m_mutex);
+	RET_TYPE ret = m_handler_pool.insert(VALUE_TYPE (addr,ptr));
+	if (ret.second == false)
+	{
+		m_handler_pool.erase(ret.first);
+	       	m_handler_pool.insert( VALUE_TYPE(addr,ptr));
+	}
+
+	return 0;
+};
+
+int service_instances::remove(const std::string& addr)
+{
+	boost::scoped_lock lock(m_mutex);
+	m_handler_pool.erase(addr);
+
+	return 0;
+};
+
+int service_instances::get_addr(std::string& addr)
+{
+	IT_TYPE it ;
+	std::string server_addr="";
+	uint32_t lowest =0xFFFFFFFFUL;
+
+	uint32_t now = time(NULL);
+	for(it = m_handler_pool.begin();it!=m_handler_pool.end();++it)
+	{
+		STATUS& status = it->second->status();
+
+		if (status.last_activity - now >DEADTIME)
+		{
+			status.status = 1;
+			continue;
+		};
+
+		if (status.load < lowest)
+		{
+			lowest = status.load;
+			server_addr = it->first;
+		}
+	};
+
+	if (lowest == 0xFFFFU)
+		return -1;
+	else
+		return 0;
+
+};
+
+int service_instances::get_all(std::vector<std::string>& status_list)
+{
+	IT_TYPE it ;
+	for(it = m_handler_pool.begin();it!=m_handler_pool.end();++it)
+	{
+		std::string info;
+		STATUS& status = it->second->status();
+		status>>info;
+		status_list.push_back(info);
+	};
+
+	return 0;
+};
+
+
+int service_instances::notify(const std::string& path,const std::string& cfg)
+{
+	IT_TYPE it ;
+	for(it = m_handler_pool.begin();it!=m_handler_pool.end();++it)
+	{
+		if (status.last_activity - now >DEADTIME)
+		{
+			it->second->notify(path,cfg);
+		};
+	};
+};
 
 };
 
--- service_config.h
+++ service_config.h
@@ -1,23 +1,26 @@
-#ifndef CONFIG_SERVER_SERVICE_H_
-#define CONFIG_SERVER_SERVICE_H_
-#include <string.h>
+#ifndef CONFIG_SERVER_SERVICE_CONFIG_H_
+#define CONFIG_SERVER_SERVICE_CONFIG_H_
 #include "atomic_operate.h"
 #include "base/singleton.h"
 #include "base/net/handler.h"
-
+#include <boost/thread/thread.hpp>
+#include <boost/thread/mutex.hpp>
+#include <string.h>
 namespace CONFIG_SERVER
 {
 
-struct status
+#define DEADTIME 180
+
+struct STATUS
 {
 	std::string addr;
 	std::string name;
 	std::string type;
 	std::string network;
-	uint8_t status;//0 服务正常 1 服务异常
-	uint16_t	load;
+	uint32_t status;//0 服务正常 1 服务异常
+	uint32_t	load;
 	uint32_t	last_activity;
-	void* data; //指向对应handler的指针
+	void operator>>(std::string info);
 };
 
 
@@ -44,14 +47,41 @@ private:
 typedef base::Singleton<ATOMIC_OPERATE<std::string,service_config_update> > CFG_UPDATE;
 
 
-struct service_instance
+class service_instances
 {
+public:
+	typedef std::map<std::string,boost::shared_ptr<base::net::handler> > POOL_TYPE;
+	typedef POOL_TYPE::iterator IT_TYPE;
+	typedef std::pair<std::string,boost::shared_ptr<base::net::handler> > VALUE_TYPE;
+	typedef std::pair<VALUE_TYPE,bool> RET_TYPE;
 
-
-	boost::shared_ptr<base::net::handler> data;
+	int insert(const std::string& addr,boost::shared_ptr<base::net::handler> ptr);
+	int remove(const std::string& addr);
+	int get_service(std::string& addr);
+	int get_all(std::vector<std::string>& status_list);
+	int notify(const std::string& path,const std::string& cfg);
+private:
+	POOL_TYPE m_handler_pool;
+	boost::mutex m_mutex;
 };
 
-typedef std::map<std::string, boost::shared_ptr<base::net::handler> > STATUS_SET;
+struct iterate_f
+{
+	virtual doit(service_instance&) =0;
+};
+
+typedef int (ITERATE_CB)(service_instance&,void*);
+class service_mgr
+{
+public:
+	service_instances& inst(const std::string& service);
+	void iterate(iterate_f*);
+private:
+	std::map<std::string,service_instances> data;
+	boost::mutex m_mutex;
+};
+
+typedef base::Singleton<service_mgr> SVCMGR; 
 
 };
 
