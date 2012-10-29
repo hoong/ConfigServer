7d1a932a0b90baa88f58e4c25cfc3a3cc5633a69
hoong <honggw@outlook.com>
2012-10-29 19:54:45 +0800

    ..................................

--- main.cpp
+++ main.cpp
@@ -15,18 +15,20 @@ int main(int argc,char**argv)
 		cmdline.addIntOption("db-info", "db information");
 		//service_engine::Service s(cmdline.parse(argc, argv));
 
+    		base::log_init(0, NULL, 20*1024*1024, 10);
+
 		LOG(info) << "\n\n====== server start ======\n\n";
 
 		service_engine::RpcStubsPool pool(
 				boost::shared_ptr<service_engine::RpcStubsPoolTraits>(new service_engine::RpcStubsMultiThreadPoolTraits(4)));
 
 		service_engine::RpcStubsManager rpc_stub(&pool);
-		rpc_stub.registerStub<config::MethodConfigGet>();
-		rpc_stub.registerStub<config::MethodConfigUpdate>();
-		rpc_stub.registerStub<config::MethodStatusRegister>();
-		rpc_stub.registerStub<config::MethodStatusRefresh>();
-		rpc_stub.registerStub<config::MethodStatusQuery>();
-		rpc_stub.registerStub<config::MethodServerAcquire>();
+		rpc_stub.registerStub<config_server::MethodConfigGet>();
+		rpc_stub.registerStub<config_server::MethodConfigUpdate>();
+		rpc_stub.registerStub<config_server::MethodStatusRegister>();
+		rpc_stub.registerStub<config_server::MethodStatusRefresh>();
+		rpc_stub.registerStub<config_server::MethodStatusQuery>();
+		rpc_stub.registerStub<config_server::MethodServerAcquire>();
 
 		service_engine::Communicator communicator;
 
